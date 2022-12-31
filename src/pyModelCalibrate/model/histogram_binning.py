78d80653225b9795535f2d0610c602d535aa2e0f
abhinash <khare.abhinash4@gmail.com>
2022-12-31 12:26:35 +0530

    added few more checks

--- src/pyModelCalibrate/model/histogram_binning.py
+++ src/pyModelCalibrate/model/histogram_binning.py
@@ -13,23 +13,7 @@ class Bin:
         self.high_prob = None  # highest probability score in the sample set.
         self.avg_prob = None  # average of the probability scores of this sample set.
 
-    @staticmethod
-    def check_values(prob: float, label: int):
-
-        if not isinstance(prob, (float, int)):
-            raise ValueError(f'variable prob contains value of incorrect datatype.\
-                Expected float found {isinstance(prob)}')
-
-        if not 0 <= prob <= 1:
-            raise ValueError(f'Value out of Bound. Expected value between 0 and 1.')
-
-        if not isinstance(label, (int,)):
-            raise ValueError(f'variable label contains value of incorrect datatype.\
-                Expected float found {isinstance(label)}')
-
     def add_sample(self, prob: float, label: int) -> None:
-
-        self.check_values(prob, label)
 
         self.samples.append((prob, label))
 
@@ -45,24 +29,50 @@ class Bin:
 class HistogramBinningCalibrator:
 
     def __init__(self, probs: list, labels: list, partition_scheme: str = 'mass'):
+
+        # check for errors
+        self.check_errors(probs, labels, partition_scheme)
+
         self.probs = probs
         self.labels = labels
         self.partition_scheme = partition_scheme
 
-        # check for errors
-        self.check_values(probs, labels)
         self.samples = [(prob, label) for prob, label in zip(probs, labels)]
         self.bins = {}
 
     @staticmethod
-    def check_values(prob: list, label: list):
-        if len(prob) != len(label):
-            raise ValueError(f'Size mismatch. prob contains {len(prob)} elements \
-                label contains {len(label)} element')
+    def check_errors(probs: list, labels: list, partition_scheme: str):
+
+        if partition_scheme not in ("mass", "width"):
+            raise ValueError(f"Incorrect parameter value provided.")
+
+        if len(probs) != len(labels):
+            raise ValueError(f'Size mismatch. prob contains {len(probs)} elements \
+                label contains {len(labels)} element')
+
+        if len(probs) == 0:
+            raise ValueError(f"prob array must contain atleast 1 element")
+
+        if len(labels) == 0:
+            raise ValueError(f"label array must contain atleast 1 element")
+
+        for prob in probs:
+            if not isinstance(prob, (float, int)):
+                raise ValueError(f'variable prob contains value of incorrect datatype.\
+                    Expected float found {type(prob)}')
+
+            if not 0 <= prob <= 1:
+                raise ValueError(f'Value out of Bound. Expected value between 0 and 1.')
+
+        for label in labels:
+            if not isinstance(label, (int,)):
+                raise ValueError(f'variable label contains value of incorrect datatype.\
+                    Expected float found {type(label)}')
 
     def fit(self):
 
         # create partition
+        probs, labels, part_ids = None, None, None
         if self.partition_scheme == 'mass':
             probs, labels, part_ids = get_uniform_mass_partitions(samples=self.samples)
         elif self.partition_scheme == 'width':
