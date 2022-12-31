483a783ab4280397fc7dfc10c14838743a204655
abhinash <khare.abhinash4@gmail.com>
2022-12-31 18:57:03 +0530

    lower bound function added for inference

---
+++ src/pyModelCalibrate/model/utils.py
@@ -0,0 +1,18 @@
+
+def strict_lower_bound(x: float, arr: list) -> int:
+
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        if arr[mid] < x:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return arr[end]
+            
+    
