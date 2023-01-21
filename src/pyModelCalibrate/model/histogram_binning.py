10b18d75db2606f106feb9f0660ba8450dff9e1d
abhinash khare <khare.abhinash4@gmail.com>
2023-01-21 13:14:18 +0530

    code refractor

--- src/pyModelCalibrate/model/histogram_binning.py
+++ src/pyModelCalibrate/model/histogram_binning.py
@@ -41,8 +41,6 @@ class HistogramBinningCalibrator:
         # check for errors
         self.check_errors(probs, labels, partition_scheme)
 
-        self.probs = probs
-        self.labels = labels
         self.partition_scheme = partition_scheme
         self.params = {key: param for key, param in kwargs.items()}
 
@@ -54,44 +52,48 @@ class HistogramBinningCalibrator:
         self.calib_array = []
 
     @staticmethod
-    def check_errors(probs: list, labels: list, partition_scheme: str):
+    def check_errors(probs: list, labels: list, partition_scheme: str, kwargs: dict):
 
-        if partition_scheme not in ("mass", "width"):
-            raise ValueError(f"Incorrect parameter value provided.")
+        assert partition_scheme in ("mass", "width", "count"), \
+            "valid partition schemes: mass, width and count"
 
-        if len(probs) != len(labels):
-            raise ValueError(f'Size mismatch. prob contains {len(probs)} elements \
-                label contains {len(labels)} element')
+        assert kwargs.keys() in ("partition_size", "width", "partition_num"), \
+            "Invalid parameter"
 
-        if len(probs) == 0:
-            raise ValueError(f"prob array must contain atleast 1 element")
+        assert len(probs) == len(labels), \
+            f'Size mismatch. probs array contains {len(probs)} elements \
+                label array contains {len(labels)} element'
 
-        if len(labels) == 0:
-            raise ValueError(f"label array must contain atleast 1 element")
+        assert len(probs) != 0, f"probs array must contain atleast 1 element"
+
+        assert len(labels) != 0, f"label array must contain atleast 1 element"
 
         for prob in probs:
-            if not isinstance(prob, (float, int)):
-                raise ValueError(f'variable prob contains value of incorrect datatype.\
-                    Expected float found {type(prob)}')
+            if not isinstance(prob, (float,)):
+                raise ValueError(f"variable prob contains value of incorrect datatype."
+                                 f"Expected float found {type(prob)}")
 
             if not 0 <= prob <= 1:
-                raise ValueError(f'Value out of Bound. Expected value between 0 and 1.')
+                raise ValueError(f"Value out of Bound. Expected value between 0 and 1.")
 
         for label in labels:
             if not isinstance(label, (int,)):
-                raise ValueError(f'variable label contains value of incorrect datatype.\
-                    Expected float found {type(label)}')
+                raise ValueError(f"variable label contains value of incorrect datatype."
+                                 f"Expected float found {type(label)}")
 
     def fit(self):
 
         # create partition
         probs, labels, part_ids = None, None, None
         if self.partition_scheme == 'mass':
-            probs, labels, part_ids = get_uniform_mass_partitions(samples=self.samples, partition_size=self.params['partition_size'])
+            probs, labels, part_ids = get_uniform_mass_partitions(samples=self.samples,
+                                                                  partition_size=self.params['partition_size'])
         elif self.partition_scheme == 'width':
-            probs, labels, part_ids = get_uniform_width_partitions(samples=self.samples, width=self.params['width'])
+            probs, labels, part_ids = get_uniform_width_partitions(samples=self.samples,
+                                                                   width=self.params['width'])
         else:
-            probs, labels, part_ids = get_uniform_num_partitions(samples=self.samples, partition_num=self.params['partition_num'])
+            probs, labels, part_ids = get_uniform_num_partitions(samples=self.samples,
+                                                                 partition_num=self.params['partition_num'])
 
         # learn bin wise statistics
         for part_id in set(part_ids):
@@ -111,8 +113,7 @@ class HistogramBinningCalibrator:
             self.low_array.append(bin.low_prob)
             self.calib_array.append(bin.calibrated_score)
 
-    @staticmethod
-    def find_bin_id(prob):
+    def find_bin_id(self, prob):
 
         idx = strict_lower_bound(prob, self.low_array)
         if idx == -1:
