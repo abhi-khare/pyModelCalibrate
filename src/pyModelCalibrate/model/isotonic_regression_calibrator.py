979470429ae5a2d60e2dbe9049a8012a0e30f356
abhinash khare <khare.abhinash4@gmail.com>
2023-01-21 13:33:56 +0530

    IsotonicRegressionCalibrator code refactored

--- src/pyModelCalibrate/model/isotonic_regression_calibrator.py
+++ src/pyModelCalibrate/model/isotonic_regression_calibrator.py
@@ -26,7 +26,7 @@ class Bin:
 
         self.samples.append((prob, label))
 
-    def compute_statistics(self) -> None:
+    def compute_bin_statistics(self) -> None:
         probs, labels = zip(*self.samples)
 
         self.low_prob, self.high_prob = min(probs), max(probs)
@@ -35,19 +35,18 @@ class Bin:
 
     def merge(self, bin_a):
         self.samples += bin_a.samples
-        self.compute_statistics()
+        self.compute_bin_statistics()
 
 
 class IsotonicRegressionCalibrator:
 
-    def __init__(self, probs: list, labels: list, partition_scheme: str = 'mass'):
+    def __init__(self, probs: list, labels: list, partition_scheme: str = 'count', **kwargs):
 
         # check for errors
-        self.check_errors(probs, labels, partition_scheme)
+        self.check_errors(probs, labels, partition_scheme, kwargs)
 
-        self.probs = probs
-        self.labels = labels
         self.partition_scheme = partition_scheme
+        self.params = {key: param for key, param in kwargs.items()}
 
         self.samples = [(prob, label) for prob, label in zip(probs, labels)]
         self.bins = {}
@@ -57,39 +56,40 @@ class IsotonicRegressionCalibrator:
         self.calib_array = []
 
     @staticmethod
-    def check_errors(probs: list, labels: list, partition_scheme: str):
+    def check_errors(probs: list, labels: list, partition_scheme: str, kwargs: dict):
 
-        if partition_scheme not in ("mass", "width"):
-            raise ValueError(f"Incorrect parameter value provided.")
+        assert partition_scheme in ("mass", "width", "count"), \
+            "valid partition schemes: mass, width and count"
 
-        if len(probs) != len(labels):
-            raise ValueError(f'Size mismatch. prob contains {len(probs)} elements \
-                label contains {len(labels)} element')
+        assert kwargs.keys() in ("partition_size", "width", "partition_num"), \
+            "Invalid parameter"
 
-        if len(probs) == 0:
-            raise ValueError(f"prob array must contain atleast 1 element")
+        assert len(probs) == len(labels), \
+            f'Size mismatch. probs array contains {len(probs)} elements \
+                label array contains {len(labels)} element'
 
-        if len(labels) == 0:
-            raise ValueError(f"label array must contain atleast 1 element")
+        assert len(probs) != 0, f"probs array must contain atleast 1 element"
+
+        assert len(labels) != 0, f"label array must contain atleast 1 element"
 
         for prob in probs:
-            if not isinstance(prob, (float, int)):
-                raise ValueError(f'variable prob contains value of incorrect datatype.\
-                    Expected float found {type(prob)}')
+            if not isinstance(prob, (float,)):
+                raise ValueError(f"variable prob contains value of incorrect datatype."
+                                 f"Expected float found {type(prob)}")
 
             if not 0 <= prob <= 1:
-                raise ValueError(f'Value out of Bound. Expected value between 0 and 1.')
+                raise ValueError(f"Value out of Bound. Expected value between 0 and 1.")
 
         for label in labels:
             if not isinstance(label, (int,)):
-                raise ValueError(f'variable label contains value of incorrect datatype.\
-                    Expected float found {type(label)}')
+                raise ValueError(f"variable label contains value of incorrect datatype."
+                                 f"Expected float found {type(label)}")
 
     @staticmethod
-    def monotonic_smoothing(bins: list) -> list:
+    def monotonic_smoothing(bins: dict) -> list:
 
         stack = []
-        for bin in bins:
+        for _, bin in bins.items():
 
             if len(stack) == 0:
                 stack.append(bin)
@@ -110,9 +110,14 @@ class IsotonicRegressionCalibrator:
         # create partition
         probs, labels, part_ids = None, None, None
         if self.partition_scheme == 'mass':
-            probs, labels, part_ids = get_uniform_mass_partitions(samples=self.samples)
+            probs, labels, part_ids = get_uniform_mass_partitions(samples=self.samples,
+                                                                  partition_size=self.params['partition_size'])
         elif self.partition_scheme == 'width':
-            probs, labels, part_ids = get_uniform_width_partitions(samples=self.samples)
+            probs, labels, part_ids = get_uniform_width_partitions(samples=self.samples,
+                                                                   width=self.params['width'])
+        else:
+            probs, labels, part_ids = get_uniform_num_partitions(samples=self.samples,
+                                                                 partition_num=self.params['partition_num'])
 
         # learn bin wise statistics
         for part_id in set(part_ids):
@@ -127,10 +132,10 @@ class IsotonicRegressionCalibrator:
         for _, bin in self.bins.items():
             bin.compute_calibrated_score()
 
-        processed_bins = self.monotonic_smoothing(self.bins)
+        merged_bins = self.monotonic_smoothing(self.bins)
 
         # compute low_bin and calib_score array. This will be used during inference.
-        for _, bin in processed_bins.items():
+        for _, bin in enumerate(merged_bins):
             self.low_array.append(bin.low_prob)
             self.calib_array.append(bin.calibrated_score)
 
@@ -146,5 +151,4 @@ class IsotonicRegressionCalibrator:
     def predict(self, probs):
 
         calibrated_probs = [self.find_bin_id(prob) for prob in probs]
-
         return calibrated_probs
